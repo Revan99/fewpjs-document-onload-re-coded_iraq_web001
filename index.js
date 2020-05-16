a2b82f3a1a20a7f5cc0ae27eb3dcf67736b2147d
Revan Sarbast <revansarbast8@gmail.com>
2020-05-16 00:54:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
   let p = document.querySelector('text');
-  p.innerHTML = 'This is really cool!'
+  
 });
+p.innerHTML = 'This is really cool!'
